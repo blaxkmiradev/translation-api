04c1eea1ee03cc4bec3babdac2d10f6905bc7500
𝓑𝓛𝓐𝓧𝓚  𝓜𝓘𝓡𝓐 <blaxkmira128@gmail.com>
2026-03-29 22:02:39 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const translateRoutes = require('./routes/translate');
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/api', translateRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
